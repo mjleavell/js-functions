df483b7c55bb74e4f94cabbeb6a2b5dd6d0b8ce7
Maggie Leavell <mjleavell@gmail.com>
2018-08-25 10:26:16 -0500

    object short hand notation

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ let counter = 0;
 counter = counter + 1;
 counter += 1;
 counter ++;
-
 // counter = 3
 // use a string @ beginning so you can see which console logs isn't working
 console.log('counter: ', counter);
+
 
 // TEMPLATE LITERAL PRACTICE (use `` for text and ${} for variables)
 const greetingElement = document.getElementById('greeting');
@@ -17,4 +17,19 @@ const firstName = 'Maggie';
 const lastName = 'Leavell';
 // greetingElement.innerHTML = 'Hello my name is ' + firstName + ' ' + lastName;
 greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
+
+// OBJECT SHORT HAND NOTATION
+let name = 'callan';
+let age = '1000000000';
+
+// let person = {
+//     name: name, 
+//     age: age
+// };
+
+// object short hand notation below
+let person = {name, age};
+// how would i console log the name?
+console.log('person:', person.name);
 
