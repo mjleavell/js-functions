937ab310e76b3e81870c4c4e04e083afbbab9643
Maggie Leavell <mjleavell@gmail.com>
2018-08-25 11:48:33 -0500

    made writeToDom function

--- functions.js
+++ functions.js
@@ -18,6 +18,7 @@ const nuggetizer2 = (animal) => {
 }
 console.log('deer:', nuggetizer2('deer'));
 
+
 // INCLASS EXERCISE
 // write a function that tells what a person is eating
 const bearSticks = nuggetizer('bear');
@@ -25,8 +26,41 @@ const bearSticks = nuggetizer('bear');
 // dont need to use actual variables for inputs; KEEP THEM GENERIC
 const nomnom = (person, food) => {
     return `${person} devoured ${food}`;
-}
+};
 
 console.log(nomnom('maggie', bearSticks)); //maggie devoured bear stix
 console.log(nomnom('zoe', 'crap')); //zoe devoured crap
 console.log(nomnom('pink','blue')); //pink devoured blue
+
+
+// INCLASS EXERCISE:
+//function that takes in a number and 
+//return the number plus 3. so input=22, output=25
+// const numberAdder = (num) => {
+//     return num + 3;
+// };
+//console.log('num: ', numberAdder(22)); //25
+//console.log('num: ', numberAdder(1000)); //1003
+//console.log('num: ', numberAdder(19431857)); //19431860
+
+
+// INCLASS EXERCISE:
+// write a function with 2 inputs that prints to DOM
+const numberAdder = (num) => {
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers');
+};
+
+// ***will use the function below for a lot of upcoming exercises
+//divId= where you want string to print
+//first input = what we are putting in; 2nd input = where we want to put text
+const printToDom = (stringToPrint, divId) => {
+  //  const selectedDiv = document.getElementById('allTheNumbers');
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
+numberAdder(22); //25
+numberAdder(2); //5
+numberAdder(5000); //5003
+printToDom('I am ready for lunch', 'feelings');
